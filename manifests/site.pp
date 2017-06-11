0f0a9d0fefef2f202cac1d66aa59798805bd0496
Daniel P <dduuch@gmail.com>
2017-06-11 21:32:42 +0000

    Added dist upgrade after adding repository

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,7 +4,11 @@ class{'profiles::apt': }
 class{'profiles::rabbitmq': }
 class{'profiles::mysql': }
 
-Apt::Source["cloud-archive-${release}"] -> Package<||>
+exec { 'dist_upgrade':
+  command => '/usr/bin/apt-get -q -y -o \'DPkg::Options::=--force-confold\' dist-upgrade',
+}
+
+Apt::Source["cloud-archive-${release}"] -> Exec['dist_upgrade'] -> Package<||>
 
 class{'openstack_integration::keystone':  }  
 class{'openstack_integration::nova':      } 
